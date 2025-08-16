a5168a4812b219c970214b439c969470b62fece9
Nico Genz <mail@nicogenz.de>
2025-08-16 09:01:26 +0200

    feat: add android implementation

--- android/src/main/java/com/caphog/sdk/CapHog.java
+++ android/src/main/java/com/caphog/sdk/CapHog.java
@@ -1,11 +1,75 @@
 package com.caphog.sdk;
 
+import android.util.Log;
+
+import com.getcapacitor.JSObject;
 import com.getcapacitor.Logger;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class CapHog {
 
-    public String echo(String value) {
-        Logger.info("Echo", value);
-        return value;
+    private String projectId;
+
+    private final OkHttpClient client = new OkHttpClient();
+
+
+    public void initialize(String projectId) {
+        this.projectId = projectId;
+    }
+
+    public void logEvent(String eventName, JSObject payload) {
+        String url = new HttpUrl.Builder()
+                .scheme("https")
+                .host("caphog.com")
+                .addPathSegment("api")
+                .addPathSegment("v1")
+                .addPathSegment("event-entries")
+                .build()
+                .toString();
+        RequestBody body = RequestBody.create(getEventData(eventName, payload).toString(), MediaType.parse("application/json; charset=utf-8"));
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e("Send analytics event", "Request failed", e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    Log.e("Send analytics event", "Event was not sent due to an error: " + response.body().string());
+                } else {
+                    Log.i("Send analytics event", "Event successfully sent");
+                }
+            }
+        });
+    }
+
+    private JSONObject getEventData(String eventName, JSObject payload) {
+        JSONObject data = new JSONObject();
+        try {
+            data.put("eventName", eventName);
+            data.put("projectId", projectId);
+            data.put("timestamp", System.currentTimeMillis());
+            data.put("customPayload", payload);
+        } catch (Exception e) {
+            Log.i("CapHog", "Error retrieving device details");
+        }
+        return data;
     }
 }
--- android/src/main/java/com/caphog/sdk/CapHogPlugin.java
+++ android/src/main/java/com/caphog/sdk/CapHogPlugin.java
@@ -1,6 +1,5 @@
 package com.caphog.sdk;
 
-import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
@@ -12,11 +11,20 @@ public class CapHogPlugin extends Plugin {
     private CapHog implementation = new CapHog();
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void initialize(PluginCall call) {
+        implementation.initialize(call.getString("projectId"));
+        call.resolve();
+    }
 
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
+    @PluginMethod
+    public void logEvent(PluginCall call) {
+        String eventName = call.getString("eventName");
+        if (eventName == null) {
+            call.reject("eventName is missing");
+            return;
+        }
+        implementation.logEvent(eventName, call.getObject("payload"));
+
+        call.resolve();
     }
 }
